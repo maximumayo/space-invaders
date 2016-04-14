6ed274905a55db3d0faf1e8344d71ebb2534838f
maximumayo <maxgmayo@gmail.com>
2016-04-13 19:38:14 -0700

    basic game working, sounds too

--- script.js
+++ script.js
@@ -4,12 +4,16 @@
         var screen = canvas.getContext('2d');
         var gameSize = {x: canvas.width, y: canvas.height};
 
-        this.bodies = [new Player(this, gameSize)];
+        this.bodies = createInvaders(this).concat(new Player(this, gameSize));
 
         var self = this;
+
+        loadSound("sounds/shoot.mp3", function (shootSound) {
+            self.shootSound = shootSound;
+        });
         var tick = function () {
             self.update();
-            self.draw(screen, gameSize)
+            self.draw(screen, gameSize);
             requestAnimationFrame(tick);
         };
         tick();
@@ -17,6 +21,14 @@
 
     game.prototype = {
         update: function () {
+            var bodies = this.bodies;
+            var notCollidingWithAnything = function (b1) {
+                return bodies.filter(function (b2) {
+                        return colliding(b1, b2);
+                    }).length === 0;
+            };
+            this.bodies = this.bodies.filter(notCollidingWithAnything);
+
             for (var i = 0; i < this.bodies.length; i++) {
                 this.bodies[i].update();
             }
@@ -31,7 +43,16 @@
 
         addBody: function (body) {
             this.bodies.push(body);
+        },
+
+        invadersBelow: function (invader) {
+            return this.bodies.filter(function (b) {
+                    return b instanceof Invader &&
+                        b.center.y > invader.center.y &&
+                        b.center.x - invader.center.x < invader.size.x;
+                }).length > 0;
         }
+
     };
 
     var Player = function (game, gameSize) {
@@ -53,9 +74,48 @@
                 var bullet = new Bullet({x: this.center.x, y: this.center.y - this.size.x / 2},
                     {x: 0, y: -6});
                 this.game.addBody(bullet);
+                this.game.shootSound.load();
+                this.game.shootSound.play();
             }
         }
     };
+
+
+    var Invader = function (game, center) {
+        this.game = game;
+        this.size = {x: 15, y: 15};
+        this.center = center;
+        this.patrolX = 0;
+        this.speedX = 0.3;
+    };
+
+    Invader.prototype = {
+        update: function () {
+            if (this.patrolX < 0 || this.patrolX > 40) {
+                this.speedX = -this.speedX;
+            }
+            this.center.x += this.speedX;
+            this.patrolX += this.speedX;
+
+            if (Math.random() > 0.995 && !this.game.invadersBelow(this)) {
+
+                var bullet = new Bullet({x: this.center.x, y: this.center.y + this.size.x / 2},
+                    {x: Math.random() - 0.5, y: 2});
+                this.game.addBody(bullet);
+            }
+        }
+    };
+
+    var createInvaders = function (game) {
+        var invaders = [];
+        for (var i = 0; i < 24; i++) {
+            var x = 30 + (i % 8) * 30;
+            var y = 30 + (i % 3) * 30;
+            invaders.push(new Invader(game, {x: x, y: y}));
+        }
+        return invaders;
+    };
+
 
     var Bullet = function (center, velocity) {
         this.size = {x: 3, y: 3};
@@ -92,6 +152,24 @@
         };
 
         this.KEYS = {LEFT: 37, RIGHT: 39, SPACE: 32};
+    };
+
+    var colliding = function (b1, b2) {
+        return !(b1 === b2 ||
+        b1.center.x + b1.size.x / 2 < b2.center.x - b2.size.x / 2 ||
+        b1.center.y + b1.size.y / 2 < b2.center.y - b2.size.y / 2 ||
+        b1.center.x - b1.size.x / 2 > b2.center.x + b2.size.x / 2 ||
+        b1.center.y - b1.size.y / 2 > b2.center.y + b2.size.y / 2);
+    };
+
+    var loadSound = function (url, callback) {
+        var loaded = function () {
+            callback(sound);
+            sound.removeEventListener('canplaythrough', loaded);
+        };
+        var sound = new Audio(url);
+        sound.addEventListener('canplaythrough', loaded);
+        sound.load();
     };
 
     window.onload = function () {
